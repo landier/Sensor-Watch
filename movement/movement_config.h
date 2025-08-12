451d59ed1d2c40af5811ec4fd7f273519968d670
Nicolas Landier <nicolas.landier@shine.fr>
2025-08-12 21:17:58 +0200

    Create landier version

--- movement/movement_config.h
+++ movement/movement_config.h
@@ -28,15 +28,30 @@
 #include "movement_faces.h"
 
 const watch_face_t watch_faces[] = {
+    // Primary faces
     simple_clock_face,
+    weeknumber_clock_face,
+    minimal_clock_face,
     world_clock_face,
-    sunrise_sunset_face,
-    moon_phase_face,
-    stopwatch_face,
+    stock_stopwatch_face,
+    timer_face,
+    day_one_face,
+    deadline_face,
+    tally_face,
+    breathing_face,
+    pulsometer_face,
+    metronome_face,
+    tuning_tones_face,
+    periodic_face,
+    minmax_face,
+    // Secondary faces
+    minute_repeater_decimal_face,
     preferences_face,
     set_time_face,
+    alarm_face,
     thermistor_readout_face,
-    voltage_face
+    voltage_face,
+
 };
 
 #define MOVEMENT_NUM_FACES (sizeof(watch_faces) / sizeof(watch_face_t))
@@ -47,7 +62,7 @@ const watch_face_t watch_faces[] = {
  * Some folks also like to use this to hide the preferences and time set faces from the normal rotation.
  * If you don't want any faces to be excluded, set this to 0 and a long Mode press will have no effect.
  */
-#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 2) // or (0)
+#define MOVEMENT_SECONDARY_FACE_INDEX (MOVEMENT_NUM_FACES - 6) // or (0)
 
 /* Custom hourly chime tune. Check movement_custom_signal_tunes.h for options. */
 #define SIGNAL_TUNE_DEFAULT
@@ -55,14 +70,14 @@ const watch_face_t watch_faces[] = {
 /* Determines the intensity of the led colors
  * Set a hex value 0-15 with 0x0 being off and 0xF being max intensity
  */
-#define MOVEMENT_DEFAULT_GREEN_COLOR 0xF
-#define MOVEMENT_DEFAULT_RED_COLOR 0x0
+#define MOVEMENT_DEFAULT_GREEN_COLOR 0x0
+#define MOVEMENT_DEFAULT_RED_COLOR 0xF
 
 /* Set to true for 24h mode or false for 12h mode */
-#define MOVEMENT_DEFAULT_24H_MODE false
+#define MOVEMENT_DEFAULT_24H_MODE true
 
 /* Enable or disable the sound on mode button press */
-#define MOVEMENT_DEFAULT_BUTTON_SOUND true
+#define MOVEMENT_DEFAULT_BUTTON_SOUND false
 
 /* Set the timeout before switching back to the main watch face
  * Valid values are:
